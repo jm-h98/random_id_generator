3753386c2d2859b3556ece50e8e9b745812ecc94
jm.h <hilgefort.jm@protonmail.com>
2021-08-19 23:51:28 +0200

    cleanup

--- generator.py
+++ generator.py
@@ -6,10 +6,7 @@ from random import randint
 import argparse
 
 
-MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genders=['Male','Female']
-
 faceNet=cv2.dnn.readNet("opencv_face_detector_uint8.pb","opencv_face_detector.pbtxt")
 ageNet=cv2.dnn.readNet("age.caffemodel","age.prototxt")
 genderNet=cv2.dnn.readNet("gender.caffemodel","gender.prototxt")
